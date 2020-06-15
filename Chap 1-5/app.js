28e0590ac21c00d143c114b07ffb24c47150cda8
SyedJazibHussainRizvi <57403257+SyedJazibHussainRizvi@users.noreply.github.com>
2020-06-15 11:56:06 -0700

    Add files via upload

---
+++ Chap 1-5/app.js
@@ -0,0 +1,242 @@
+// Chapter 1 
+
+
+// (1)
+// alert("Welcome to this Page");
+
+// (2)
+// alert("Error! Please enter a valid password");
+
+// (3)
+// alert("Welcome to JS Land... \n Happy Coding!");
+
+// (4)
+// alert("Welcome to JS Land...")
+// alert("Happy Coding!")
+
+// (5)
+// console.log ("Hello... I can run JS through my browser’s console")
+
+// (7)
+// All
+
+
+// Chapter 2
+
+
+// (1)
+// var username="Jazib";
+// alert(username);
+
+// (2)
+// var username="Jazib";
+// alert(username);
+
+// (3)
+// var message;
+// message="hello world";
+// alert (message);
+
+// (4)
+// var studentname="Jazib";
+// alert(studentname);
+
+// var StudentAge="19 Years Old";
+// alert(StudentAge)
+
+// var StudentCourse="Web and App Course";
+// alert(StudentCourse);
+
+// (5)
+// alert("PIZZA\nPIZZ\nPIZ\nPI\nP");
+
+// (6)
+// var email = "jazibhussain110@gmail.com";
+// alert("My Email Address is:" +email);
+
+// (7)
+// var book="A smaeter\n way to learn Javascript";
+// alert("I am trying to learn from the book" +book);
+
+// (8)
+// document.write("Yah ! I can write HTML content through JavaScript");
+
+// (9)
+// var a ="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+// alert(a);
+
+
+// Chapter 3
+
+  
+// (1)
+// var age = "I am 19 Years old";
+// alert(age);
+
+
+// (2)
+// var numberOfvisits ="You have visited this site 14 times";
+// alert(numberOfvisits);
+
+// (3)
+// var birthYear =2000;
+// document.write("My Birth Year Is" +" " +birthYear+ "<br>" + "Data Type OF My Declared Variable Is Number");
+// document.write("<br><br>");
+
+// (4)
+// document.write("<b>" + "John Doe" + "</b>" + " " + "ordered" + " " + "<b>" + "5" + "  " + "T-shart" +"</b>" + "(s)" + " " + "on XYZ Clothing store");
+
+
+
+// Chapter 4
+
+
+// (1)
+// var variable1 = "Hello World!" + " ",
+//     variable2 = "Jazib Testing..." + " ",
+//     variable3 = 20;
+// alert(variable1 + variable2 + variable3);
+
+
+// (2)
+
+//Illegal Variable Names
+//var _9w;
+//var %jaz;
+//-conting = 24;
+//var ,lastname = "jaz";
+//Gethours;
+
+//Legel Variable Names
+//var jazJazib;
+//var $_;
+//var booHoo;
+//var firstLegalChar = "1";
+//var topScore = 100;
+//var total = cost + profit;
+
+
+// (3)
+// document.write("<p style='font-size:30px;'>" + "Rules for naming JS variables" + "</p>" + "<br/>");
+// document.write("<p>" + "Variable names can only contain" + " " + "," + " " + "numbers" + "," + " " + "$ and" + " " + " _" + " " + "." + " " + "For example :"+  " " + "$my" + " " + "_ 1st Variable" + "<br/>" + "Variables must begin with a letter, $ or _ . For Example: $name, _ name or name" + "</br>" + "Variable names are case" + " " + "sensitive" + "</br>" + "Variable names should not be JS" + " " + "Keywords" + "</p>");
+
+
+// Chapter 5
+
+
+// (1)
+// var a = 3;
+// var b = 5;
+// var c = a + b;
+// document.write("<h3>" + "Sum of" + "  " + a + " " + "and" + " " + b + " " + "is" + " " + c + "</h3>");
+
+
+// (2)
+//for subtraction
+// var a = 3;
+// var b = 5;
+// var c = a - b;
+// document.write("<h3>" + "Sum of" + "  " + a + " " + "and" + " " + b + " " + "is" + " " + c + "</h3>");
+
+//for multiplication
+// var a = 3;
+// var b = 5;
+// var c = a * b;
+// document.write("<h3>" + "Sum of" + "  " + a + " " + "and" + " " + b + " " + "is" + " " + c + "</h3>");
+
+//for  division
+// var a = 3;
+// var b = 5;
+// var c = a / b;
+// document.write("<h3>" + "Sum of" + "  " + a + " " + "and" + " " + b + " " + "is" + " " + c + "</h3>");
+
+//for  modulus
+// var a = 3;
+// var b = 5;
+// var c = a % b;
+// document.write("<h3>" + "Sum of" + "  " + a + " " + "and" + " " + b + " " + "is" + " " + c + "</h3>");
+
+
+
+// (3)
+// var a;
+// document.write("<p style='font-size: 20px; font-weight: 500;'>" + "Value after variable declaration is" + "  " );
+// document.write("<p style='font-size: 20px; font-weight: 500;'>" + "Initial Value:" + "  "  +  " ");
+// document.write("<p style='font-size: 20px; font-weight: 500;'>" + "Value after increment is:" + "  " );
+// document.write("<p style='font-size: 20px; font-weight: 500;'>" + "Value after addition is:" + "  " );
+// document.write("<p style='font-size: 20px; font-weight: 500;'>" + "Value after decrement is:" + "  ");
+// document.write("<p style='font-size: 20px; font-weight: 500;'>" + "The remainder is:" + "  ");
+
+
+// (4)
+// var ticketPrice = 600;
+// var b = 5;
+// var c = ticketPrice * b;
+// document.write("<h3>" + "Total cost to buy" + "  " + b + " " + "tickets to a movie is" + " " + c + "PKR"+ "</h3>");
+
+
+
+// (5)
+// var x = 4, i;
+// for(i=1;i<=10;i++)
+// document.write("<h3>"  + "4x" + i + "=" + i*x + "</h3>");
+
+
+// (6)
+// var celSius=30, fahreNheit=86, conversion1, conversion2;
+// conversion1=(fahreNheit-32)*5/9;
+// conversion2=(celSius*9/5)+32
+// document.write("Fahrenheit Is:"+ " " + conversion1 + "<br/>" + "Celsius Is:" + " " + conversion2);
+
+
+// (7)
+// var  priceOfitem1 = 650,  priceOfitem2 = 100, quantity1 = 3, quantity2 = 7, shippingCharges = 100, 
+// totalCost =priceOfitem1*quantity1+priceOfitem2+shippingCharges*quantity2;
+// document.write("<h1>"+ "Shopiing Cart " + "</h1>" + "<br/>");
+// document.write("<p style='font-size: 20px; font-weight: 500;'>" + " Price of item 1 is" + " " + priceOfitem1 + "<br/>" + "Quantity of item 1 is" + " " + quantity1 + "<br/>" + "Price of item 2 is" + " " + priceOfitem2 + "<br/>" + "Quantity of item 2 is" + " " + quantity2 + "<br/>" + " Sipping Charges" + " " + shippingCharges + "<br/>" +  "<br/>" + "Total cost of your ordered is" + " " + totalCost + "</p>");
+
+
+
+// (8)
+// document.write("<h1>"+ "Marks Sheets " + "</h1>" + "<br/>");
+// var total = "980", ob = "804", per;
+// per=ob*100/total;
+// document.write("<p style='font-size: 20px; font-weight: 500;'>" + "Total Marks:" + " " + total + "<br/>" + "Marks Obtained:" + " " + ob + "<br/>" + "Percentage" + " " + per + "</p>");
+
+
+
+//(9)
+// var usDollars = 104.80*10, saudiRiyals = 25*28, pakistaniRupee;
+//  pakistaniRupee= usDollars+saudiRiyals;
+// document.write("<h1>"+ "Currency In PKR" + "</h1>" + "<br/>");
+// document.write("<p style='font-size: 20px; font-weight: 500;'>" + "Total Currency in PKR:" + " " + pakistaniRupee + "</p>");
+
+
+// (10)
+// a=5, b=2, c=a*b, d=c/a;
+// document.write("<p style='font-size: 20px; font-weight: 600;'>" + "a. Add" + " " + a + "<br/>" + "b. Multiply by" + " " + c + "<br/>" + "c. Divide the result by" + " " + d + "</p>");
+
+
+// (11)
+// document.write("<h1 style='font-size: 38px; font-weight: 900;'>" + "Age Calculator" + "</h1>" + "<br/>");
+// var currentYear = 2020, birthYear = 1999, age=currentYear-birthYear;
+// document.write("<p style='font-size: 22px; font-weight: 500;'>" + "Current Year: " + " " + currentYear + "<br/>" + "Birth Year:" + " " + birthYear + "<br/>" + "Your Age Is:"+ " " + age);
+
+
+
+// (12)
+// var area, r, pi = 3.142, coc;
+// r=parseInt(prompt("Enter The Radius"));
+// area=pi*r*r;
+// coc=2*pi*r;
+// document.write("<h1 style='font-size: 38px; font-weight: 900;'>" + "The Geometrizer" + "</h1>" + "<br/>");
+// document.write("<p style='font-size: 22px; font-weight: 500;'>" + "Radius of a circle: " + " " + r + "<br/>" + "The circumference is:" + " " + coc +  "<br/>" + "The area is:" + " " + area + "</p>");
+
+
+
+// (13)
+// var favoriteSnack="Potato Chips", currentAge=21,  maximumAge=70,  amountPerday=2, totalForrest;
+// totalForrest = currentAge+maximumAge*amountPerday;
+// document.write("<h1 style='font-size: 38px; font-weight: 900;'>" + "The Lifetime Supply Calculator" + "</h1>" + "<br/>");
+// document.write("<p style='font-size: 20px; font-weight: 500;'>" + "Favourite Snack:"  + " " + favoriteSnack + "<br/>" + "Current Age:" + " " + currentAge + "<br/>" + "Extimated Maximum Age:"+ " " + maximumAge + "<br/>" + "Amount Of Snacks Per Day" + " " + amountPerday + "<br/>" + "You will need" + " " + totalForrest + " " + "to last you until the ripe old age of" + "  " + maximumAge + "</p>" );
+
---
+++ Chap 17-20/app.js
@@ -0,0 +1,115 @@
+// (1)
+var mul_arr=[["jaz"],20,"b"]
+alert(mul_arr);
+
+
+// (2)
+ var arr1=[[0 , 1 , 2 , 3],[1,0,1,2],[2,1,0,1]];
+ document.write(arr1[0]+"<br>"+arr1[1]+"<br>"+arr1[2]);
+
+
+// (3)
+ for(var i=1;i<=10;i++){
+     document.write(i +"<br>");
+ }
+
+
+// (4)
+ var a=prompt("Entre a number to show its multiplication table");
+ var b=prompt("Entre length of multiplication table");
+ var x=a;
+ var y=b;
+ for(var i=1;i<=b;i++){
+     document.write(a+"x"+i+"="+a*i+"<br>");
+ }
+ 
+
+// (5)
+ var fruits = ["apple", "banana", "mango", "orange","strawberry"];
+ for(var i=0;i<fruits.length;i++){
+   document.write(fruits[i]+"<br>");
+    
+ }
+for(var i=0;i<fruits.length;i++){
+    document.write("Element at index "+i+" is "+fruits[i]+"<br>");
+    
+
+ }
+
+
+// (6)
+ document.write("Counting: <br><br>");
+ for(var i=1;i<=15;i++){
+    document.write(i+",");
+ }
+
+//part
+document.write("<br><br>Reverse Counting: <br><br>");
+ for(var i=10;i>=1;i--){
+     document.write(i+",");
+}
+
+ document.write("<br><br>Even <br><br>");
+ for(var i=0;i<=20;i+2){
+     document.write(i+",");
+ }
+
+ document.write("<br><br>Odd <br><br>");
+ for(var i=1;i<=20;i+2){
+     document.write(i+",");
+ }
+
+ document.write("<br><br>Series <br><br>");
+for(var i=2;i<=20;i+2){
+     document.write(i+"k,"); 
+ }
+
+
+
+// (7)
+ var array=["cake","apple pie","cookie","chips","patties"]
+ var a=prompt();
+ for(var i=0;i<array.length;i++){
+     if(a===array[i]){
+         alert(a+"is available at index "+i+" in our bakery");
+     }
+     else{
+         alert("we are sorry"+a+" is not available in our bakery");
+    }
+
+ }
+
+
+// (8)
+ var array = [3 , 6, 2, 56, 32, 5, 89, 32];
+ var largest= 0;
+ document.write(array);
+
+ for (i=0; i<=largest;i++){
+   if (array[i]>largest) {
+         var largest=array[i];
+     }
+ }
+ document.write("<br>"+largest);
+
+
+
+// (9)
+ var array = [3 , 6, 2, 56, 32, 5, 89, 32];
+ var smallest= 100;
+ document.write(array+"<br>");
+
+ for (i=0; i<=smallest;i++){
+     if (array[i]<smallest) {
+         var smallest=array[i];
+     }
+ }
+ document.write(smallest);
+
+
+// (10)
+ for(var i=1;i<=100;i++){
+     if(i%5==0){
+         document.write(i+" ,")
+     }
+}
